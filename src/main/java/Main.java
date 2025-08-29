0a0cf37dd14af4a2b9adb8f82a1e6c495978d7c2
Ayushman Garg <ayushmangarg22@gmail.com>
2025-08-29 12:09:45 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-      int port = 6379; // Default Redis port
+      Integer port = 6379; // Default Redis port
+      if(args.length>=2) {
+        port = Integer.parseInt(args[1]);
+      }
       RedisServer redisServer = new RedisServer(port);
       redisServer.start();
   }
