be3963cfd307b4216e7194fc5c83662476d22687
Ayushman Garg <ayushmangarg22@gmail.com>
2025-08-30 11:27:01 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -47,7 +47,11 @@ public class Main {
         slave.getOutputStream().write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n".getBytes());
         slave.getOutputStream().flush();
         readLineFrom(in);
-        slave.close();
+        // *3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n
+        slave.getOutputStream().write("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n".getBytes());
+        slave.getOutputStream().flush();
+        readLineFrom(in);
+        // slave.close();
       }
     }
 
